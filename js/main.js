e88ab737abc1ac078bca924bc9f6940c1babadbd
steve <stevechoi07@gmail.com>
2025-09-10 22:46:48 +0900

    firebase 살아있다

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // js/main.js
-import { ui } from './ui.js'; // ui.js를 불러옵니다.
+import { ui } from './ui.js';
+import { db } from './firebase.js'; // firebase.js를 불러옵니다.
 
-console.log('main.js가 ui.js와 연결 후에도 살아있다!', ui); // ui 객체도 함께 출력해봅니다.
+console.log('main.js가 모든 파일과 연결 후에도 살아있다!');
