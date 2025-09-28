3dac1cbca8991ea36ff27ebf8fa57b5ef523b5d0
steve <stevechoi07@gmail.com>
2025-09-28 16:53:13 +0900

    app.js update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// App.js
-
-// React 라이브러리를 불러옵니다.
-const { useState } = React;
-
-// 우리가 만든 FormBuilderAdmin 컴포넌트를 불러옵니다.
-// 중요: 확장자(.js)까지 모두 적어주세요!
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import FormBuilderAdmin from './features/form-builder/FormBuilderAdmin.js';
 
 // index.html에 있는 'root' div를 선택합니다.
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
 // 'root' div 안에 우리 앱(FormBuilderAdmin 컴포넌트)을 그려줍니다.
-root.render(<FormBuilderAdmin />);
+root.render(
+  <React.StrictMode>
+    <FormBuilderAdmin />
+  </React.StrictMode>
+);
